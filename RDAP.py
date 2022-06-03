fc3f51bde136cfb6d83db53c4075b676585bef50
ethan <eschuh12@gmail.com>
2022-06-03 15:36:54 -0700

    error handling

--- RDAP.py
+++ RDAP.py
@@ -6,13 +6,26 @@ def rdapLookup_b(ip):
     url = f'https://rdap.arin.net/registry/ip/{ip}'
     x = requests.get(url, headers={'accept': 'application/json'})
     y = x.json()
-    print(y["handle"])
+    try:
+        print(y["handle"])
+    except KeyError:
+        print("Handle not available")
     print(y["startAddress"])
     print(y["endAddress"])
     print(y["ipVersion"])
-    print(y["name"])
-    print(y["type"])
-    print(y["parentHandle"])
+    try:
+        print(y["name"])
+    except KeyError:
+        print("name not available")
+    try:
+        print(y["type"])
+    except KeyError:
+        print("type not available")
+    try:
+        print(y["parentHandle"])
+    except KeyError:
+        print("parentHandle not available")
+
 
 
 def rdapLookup_d(ip):
@@ -20,5 +33,3 @@ def rdapLookup_d(ip):
     x = requests.get(url, headers={'accept': 'application/json'})
     y = json.loads(x.text)
     print(json.dumps(y, indent=2))
-
-
